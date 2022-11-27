da7d9bdf584a4c93f7d6d7d6dd9b20c47eb01674
Elliott <ecmagnuson@wisc.edu>
2022-11-27 14:03:28 -0600

    added all to accept @ and + arg

--- cmd/all.go
+++ cmd/all.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"godo/utils"
+	"log"
 
 	"github.com/spf13/cobra"
 	"gorm.io/driver/sqlite"
@@ -10,7 +11,7 @@ import (
 )
 
 //lsAllLocations lists out all of the locations currently in use in the database
-func lsAllLocations() {
+func lsAllLocations(args []string) {
 	db, err := gorm.Open(sqlite.Open(utils.TodoDBPath()), &gorm.Config{
 		//Logger: logger.Default.LogMode(logger.Info),
 	})
@@ -18,26 +19,41 @@ func lsAllLocations() {
 		panic("failed to connect database")
 	}
 
+	context := args[0] // search by "@" or by "+"
 	var tasks []Task
-
-	//SELECT DISTINCT `location` FROM `tasks` WHERE `todo` = 1 ORDER BY location asc
-	db.Distinct("location").Order("location asc").Where("todo", 1).Find(&tasks)
-
-	for _, task := range tasks {
-		fmt.Println(task.Location)
+	if context == "@" {
+		//SELECT DISTINCT `location` FROM `tasks` WHERE `todo` = 1 ORDER BY location asc
+		db.Distinct("location").Order("location asc").Where("todo", 1).Find(&tasks)
+		for _, task := range tasks {
+			fmt.Println(task.Location)
+		}
+	} else if context == "+" {
+		//SELECT DISTINCT `project` FROM `tasks` WHERE `todo` = 1 ORDER BY project asc
+		db.Distinct("project").Order("project asc").Where("todo", 1).Find(&tasks)
+		for _, task := range tasks {
+			if task.Project == "+" {
+				continue
+			}
+			fmt.Println(task.Project)
+		}
 	}
 }
 
-// lscCmd represents the lsc command
-var lscCmd = &cobra.Command{
-	Use:   "lsc",
-	Short: "list all locations currently in use",
+// allCmd represents the all subcommand
+var allCmd = &cobra.Command{
+	Use:   "all",
+	Short: "list all locations or projects currently in use",
 	Long:  "list all locations currently in use",
 	Run: func(cmd *cobra.Command, args []string) {
-		lsAllLocations()
+		if len(args) == 0 {
+			log.Fatalf("implementing listing all later.")
+			//list out all combinations of @ and + togethor
+		}
+		lsAllLocations(args)
 	},
 }
 
+//all is a subcommand of ls
 func init() {
-	rootCmd.AddCommand(lscCmd)
+	lsCmd.AddCommand(allCmd)
 }
